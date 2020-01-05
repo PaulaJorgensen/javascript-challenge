e177b43006b60f06c9b84b866f5e4ca8b1a3a4ca
Paula Jorgensen <53984747+PaulaJorgensen@users.noreply.github.com>
2020-01-04 19:17:28 -0500

    Update app.js

--- static/js/app.js
+++ static/js/app.js
@@ -4,6 +4,7 @@ var tableData = data;
 // YOUR CODE HERE!
 var tbody = d3.select("tbody");
 //loop through 'tableData' and console.log each UFO siting object
+function tablebuild(tableData){
 tableData.forEach(function(ufoSighting) {
      var row = tbody.append("tr");
     Object.entries(ufoSighting).forEach(function([key,value]){
@@ -12,17 +13,20 @@ tableData.forEach(function(ufoSighting) {
         cell.text(value);
     });
 });
+};
+tablebuild(tableData);
+
   // 'Filter Table' button
 var button = d3.select("#filter-btn");
 
 button.on("click", function(event) {
-
+    var tbody = d3.selectAll('tbody');
+    tbody.selectAll("*").remove();
     var filteredData = tableData;
     var inputId = document.getElementsByClassName("form-control");
   
   // iterate through all the input fields
     for (var i = 0; i < inputId.length; i++) {
-	
         var idName = inputId[i].id;
         var field = d3.select("#" + idName).property("value");
     
@@ -31,6 +35,9 @@ button.on("click", function(event) {
             var filteredData = filteredData.filter(ufoSighting =>
             ufoSighting[idName] ===field);
         };
+    
     };
     console.log(filteredData);
+    tablebuild(filteredData);
+
 });
